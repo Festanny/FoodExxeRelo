8ffa9fa12d56cb21ec4509b12162fdd198479826
Festanny <60522731+Festanny@users.noreply.github.com>
2023-03-07 12:54:13 +0400

    upd

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $('.menuSection .menuBlock .menu-list .item .img').height(
     $('.menuSection .menuBlock .menu-list .item .img').width()*9/16
 )
+
+new Swiper(".gallerySlider", {
+    slidesPerView: 3,
+    spaceBetween: 24,
+    navigation: {
+        nextEl: ".gallerySlider .swiper-button-next",
+        prevEl: ".gallerySlider .swiper-button-prev",
+    },
+});
+
+$('.menuListSection .menuListBlock .product .gallery .swiper .img img').on('click', function() {
+    let link = $(this).attr('src'),
+        linkMainPhoto = $('.menuListSection .menuListBlock .product .gallery > .img img')
+    linkMainPhoto.attr('src', link)
+})
+
+
+
